a89d21e3b0606fff134202f6381fca28ff77de29
Aman Kumar <amanalphakumar@gmail.com>
2020-04-24 15:24:30 +0530

    Create Santa_Claus_and_Candies.cpp

---
+++ S/Santa_Claus_and_Candies.cpp
@@ -0,0 +1,72 @@
+// https://codeforces.com/gym/277514/problem/A
+// Uses backtracking.
+
+#include <bits/stdc++.h>
+
+using namespace std ;
+
+
+int distribute( int left, int last )
+{
+	if( left == 1 )
+		return 1 ;
+		
+	else
+	{
+		++last ;
+		
+		if( last <= left )
+			return last ;
+		else
+			return last - 1 ;
+	}
+		
+}
+
+vector<int> santa( int left )
+{
+	map<int,bool> chosen ;
+	vector<int> ret ; 
+	 
+	int last = 0 ;
+	
+	while( left > 0 )
+	{
+		int candy = distribute( left, last ) ;
+		
+		auto iter = chosen.find( candy ) ;
+		
+		if( iter == chosen.end() )
+		{
+			ret.push_back( candy ) ;
+			chosen.insert( pair<int,bool>( candy, false ) ) ;
+			
+			last = candy ;
+			left = left - candy ;
+		}
+		else
+		{
+			last = ret[ ret.size() -1 ] ;
+			ret.pop_back() ;
+			left = left + last ;
+		}
+	}
+	
+	return ret ;
+}
+
+int main()
+{
+	int n ;
+	cin >> n ;
+	
+	vector<int> seq = santa( n ) ;
+	
+	int sum = 0 ;
+	
+	cout << seq.size() << "\n" ;
+	for( int i = 0 ; i < seq.size() ; i++ )
+		cout << seq[i] << " " ;
+		
+	return 0 ;
+}
