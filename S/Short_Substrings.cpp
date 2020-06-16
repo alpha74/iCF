abc89fe5d5b9cbefef93b47f56388a278d85dc90
Aman Kumar <amanalphakumar@gmail.com>
2020-06-17 00:06:17 +0530

    Create Short_Substrings.cpp

---
+++ S/Short_Substrings.cpp
@@ -0,0 +1,48 @@
+```
+// https://codeforces.com/contest/1367/problem/A
+// Short Substrings
+#include <bits/stdc++.h>
+
+using namespace std ;
+
+string geta( string &b )
+{
+	int bsize = b.length() ;
+	
+	if( bsize <= 2 )
+	{
+		string a = b ;
+		return a ;
+	}
+	else
+	{
+		// Delete every 2 char
+		string a = "" ;
+		
+		for( int i = 0 ; i < bsize ; i+= 2 )
+		{
+			a = a + b[i] ;
+		}
+		a += b[ bsize -1 ] ;
+		return a ;
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio( 0 ) ;
+	cin.tie( 0 ) ;
+	
+	int t ;
+	cin >> t ;
+	
+	while( t-- )
+	{
+		string b ;
+		cin >> b ;
+		
+		cout << geta( b ) << "\n" ;
+	}
+	return 0 ;
+}
+```
