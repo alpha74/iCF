3a780ca963e0fdc852d1444125c8e0c3b3013405
Aman Kumar <amanalphakumar@gmail.com>
2020-07-01 22:44:18 +0530

    Create Magical_Sticks.cpp

---
+++ M/Magical_Sticks.cpp
@@ -0,0 +1,32 @@
+// https://codeforces.com/contest/1371/problem/A
+
+#include <bits/stdc++.h>
+
+using namespace std ;
+
+#define ll long long int
+
+ll getSameSticks( ll n )
+{
+	
+	return (n+1)/2 ;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio( 0 ) ;
+	cin.tie( 0 ) ;
+	
+	int t ;
+	cin >> t ;
+	
+	while( t-- )
+	{
+		ll n ;
+		cin >> n ;
+		
+		cout << getSameSticks( n ) << "\n" ;
+	}
+	
+	return 0 ;
+}
