31a1b5b3a450047bb3cae131009dd0cb8abd28af
Aman Kumar <amanalphakumar@gmail.com>
2020-07-17 22:21:20 +0530

    Create Make_It_Good.cpp

---
+++ M/Make_It_Good.cpp
@@ -0,0 +1,54 @@
+// Make It Good
+// Find indices where order changes
+// https://codeforces.com/contest/1385/problem/C
+
+#include <bits/stdc++.h>
+
+using namespace std ;
+
+#define ll long long int
+
+int main()
+{
+	ios_base::sync_with_stdio( 0 ) ;
+	cin.tie( 0 ) ;
+	
+	int t ;
+	cin >> t ;
+	
+	while( t-- )
+	{
+		int n;
+		cin >> n ;
+	
+		vector<ll> a ;
+	
+		for( int i = 0 ; i < n ; i++ )
+		{
+			int temp ;
+			cin >> temp ;
+			a.push_back( temp ) ;
+		}
+		
+		int p = n-1 ;
+		
+		// Do till an element <= current element is found backwards
+		while( p-1 >= 0 && a[p-1] >= a[p] )
+		{
+			p-- ;
+		}
+		
+		// Do till an element >= currrent element is found backwards
+		while( p-1 >= 0 && a[p-1] <= a[p] )
+		{
+			p-- ;
+		}
+		
+		// Print output
+		cout << p << "\n" ;
+		
+	}
+	
+	return 0 ;
+}
+
